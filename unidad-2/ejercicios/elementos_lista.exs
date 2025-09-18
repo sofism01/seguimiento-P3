be58b12f7dd1c1d14eddea08974e19e5ddb4f91b
sofia <167391225+sofism01@users.noreply.github.com>
2025-09-18 17:41:55 -0500

    feat: avance de ejercicios recursividad

---
+++ unidad-2/ejercicios/elementos_lista.exs
@@ -0,0 +1,26 @@
+defmodule Main do
+@moduledoc """
+Módulo para contar elementos en una lista de forma recursiva.
+"""
+
+@doc """
+Función principal que ejecuta el conteo de elementos en una lista de ejemplo.
+"""
+  def main do
+    lista = [1, 2, 3, 4, 5]
+    cantidad = contar_elementos(lista)
+    IO.puts("La lista tiene #{cantidad} elementos.")
+  end
+
+  @doc """
+  Cuenta el número de elementos en una lista de forma recursiva.
+  """
+  # Cláusula para lista vacía
+  def contar_elementos([]), do: 0 #caso base
+
+  # Cláusula para lista no vacía (caso recursivo)
+  def contar_elementos([_ | resto]), do: 1 + contar_elementos(resto) #llamado recursivo
+
+end
+
+Main.main()
